2c19f05fd15f678b99e8721efa3476ade3e1e8bd
C0deGenin <112136466+C0deGenin@users.noreply.github.com>
2022-08-26 00:18:17 +0000

    Create cart.php

---
+++ cart.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>CART</title>
+</head>
+<nav class="menu app_flex">
+    <h1>YOUR CART</h1>
+    <div class="btns app_flex">
+        <span class="opt">
+            <a href="index.php" >Home</a>
+        </span>
+        <span class="opt">
+            <a href="">Log In</a>
+        </span>
+    </div>
+</nav>
+<body class="body app_flex">
+    <h2>Your cart</h2>
+    
+    <form action="" method="post" class="app_flex">
+        <div class="products-list app_flex">
+
+                <div class="product app_flex">
+                    <span>
+                        Burger
+                    </span>
+                    <button class="product-add app_flex" >
+                        x
+                    </button>
+                </div>
+          
+        </div>
+    </form>
+
+</body>
+</html>
