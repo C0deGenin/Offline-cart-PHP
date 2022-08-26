f84b19be78f5166db447253d740ca712b04d4c20
C0deGenin <112136466+C0deGenin@users.noreply.github.com>
2022-08-26 00:15:26 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>Home</title>
+</head>
+<nav class="menu app_flex">
+    <h1>HOME PAGE</h1>
+    <div class="btns app_flex">
+        <span data-count="4" class="opt">
+            <a href="cart.php" >Cart</a>
+        </span>
+        <span class="opt">
+            <a href="">Log In</a>
+        </span>
+    </div>
+</nav>
+<body class="body app_flex">
+    <h2>Simple products</h2>
+    
+    <form action="" method="post" class="app_flex">
+        <div class="products-list app_flex">
+          
+                <div class="product app_flex">
+                    <span>
+                        Burger
+                    </span>
+                    <button class="product-add app_flex">
+                        +
+                    </button>
+                </div>
+        </div>
+    </form>
+    
+</body>
+</html>
